c62bd1f496183e3ac81eeae1df17f683fa6fc25b
Ale <74743834+Laserr994@users.noreply.github.com>
2023-06-30 19:41:51 -0300

    Add files via upload

---
+++ holamundo.py
@@ -0,0 +1,22 @@
+#HolaMundo, uno de mis primeros proyectos usando solamente funciones print() y input()
+
+print("=========================================")
+print("                  HolaMundo!!            ")
+print("=========================================")
+print("Como te llamas?: ")
+
+mi_nombre = input()
+print("Es un placer de conocerte!... " + mi_nombre)
+
+print("y dime, a que te dedicas, " + mi_nombre + "?")
+dedicacion_verbo = input("me dedico a: ")
+
+print("muy buena ocupación " + mi_nombre + "!")
+print("te dedicas a: " + dedicacion_verbo)
+
+print("y cuales son tus hobbys preferidos?: ")
+
+hobbys_principales = input("Mis hobbys preferidos son: ")
+
+print("Genial!, tus hobbys son: " + hobbys_principales)
+
